5161c1b31ebc04aa5ec4f436db5fbfd0a40d5861
jrg96 <gomezlopez.jorge96@gmail.com>
2020-01-13 16:24:34 -0600

    Seguridad a traves de usuarios de DB activada

--- src/main/resources/static/db.sql
+++ src/main/resources/static/db.sql
@@ -1,6 +1,6 @@
 create table users(
     username varchar2(50) not null primary key,
-    password varchar2(50) not null,
+    password varchar2(60) not null,
     enabled number(1) not null
 );
 
@@ -9,3 +9,5 @@ create table authorities (
     authority varchar2(50) not null,
     constraint fk_authorities_users foreign key(username) references users(username)
 );
+
+INSERT INTO users VALUES ('luis','$2a$10$O4zV8CJ.E6KyrwuRocn4ceIoYRB/AjwCA6BbGs98a3WW3GUiKXkZy',1); --luis123
